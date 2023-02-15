b3e79f12ede54399052bbe5f25c71f945696a83e
Mayank Chauhan <bobmayank1999@gmail.com>
2023-02-15 22:14:16 +0530

    second commit

--- src/components/SelectVisibility.jsx
+++ src/components/SelectVisibility.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const SelectVisibility = ({handleVisibility}) => {
     return (
-        <select name="" id="" className="border rounded-md indent-2 w-32" onChange={handleVisibility} defaultValue="all">
-            <option value="all">Show all</option>
-            <option value="completed">Completed</option>
-            <option value="active">Active</option>
+        <select name="" id="" className="border rounded-md indent-2 w-32" onChange={handleVisibility} defaultValue="SHOW_ALL">
+            <option value="SHOW_ALL">All</option>
+            <option value="SHOW_COMPLETED">Completed</option>
+            <option value="SHOW_ACTIVE">Active</option>
         </select>
     )
 }
