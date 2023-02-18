351acae77387d67b008f88f2073b06d6da81b688
Mayank Chauhan <bobmayank1999@gmail.com>
2023-02-18 12:02:06 +0530

    enhance2 done

---
+++ src/selectors/index.js
@@ -0,0 +1,7 @@
+// selectors 
+export const todosSelector = (state) => {
+    return state.todos;
+}
+export const visibilityFilterSelector = (state) => {
+    return state.visibilityFilter;
+}
---
+++ src/sub-reducers/todos.js
@@ -0,0 +1,33 @@
+const todo = (state = {}, action) => {
+    switch (action.type) {
+      case "ADD_TODO":
+        return {
+          id: action.id,
+          text: action.text,
+          completed: action.completed,
+        }
+      case "TOGGLE_TODO":
+        return {
+          ...state, completed: !state.completed,
+        }
+      default:
+        return state;
+    }
+  }
+
+
+export const todos = (state = [], action) => {
+    switch (action.type) {
+      case "ADD_TODO":
+        return [
+          ...state,
+          todo(undefined, action)
+        ]
+      case "TOGGLE_TODO":
+        return state.map((t) => {
+          return t.id === action.id ? todo(t, action) : t;
+        })
+      default:
+        return state
+    }
+  }
